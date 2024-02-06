d9c1344737ca71aa78f3aea1c94bb8c4ae3ef35f
jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>
2024-02-06 20:07:17 +0000

    Move csv parsing code to separate file, and create file for interacting with Google Maps API

--- index.ts
+++ index.ts
@@ -1,54 +1,9 @@
-import { parse } from "./deps.ts";
 import { loadEnv } from "./deps.ts";
+import { parseHospitalData } from "./parseCsv.ts";
+import { searchForPlace } from "./googleMaps.ts";
 
 await loadEnv({ export: true });
 
-const hospitalsCsv = await Deno.readTextFile("./data/hospitals.csv");
+const hospitals = await parseHospitalData();
 
-const hospitalObjects = parse(hospitalsCsv, {
-  skipFirstRow: true,
-});
-
-type GeoLocation = {
-  latitude: number;
-  longitude: number;
-};
-
-type Hospital = {
-  name: string;
-  city: string;
-  state: string;
-  address: string;
-  zipCode: string;
-  type: string;
-  location?: GeoLocation; // Get from Google Maps API
-  cmsId: string;
-  imageUrl?: string; // Get from Google Maps, upload to Supabase storage, and add here
-  slug: string;
-};
-
-function toTitleCase(str: string) {
-  return str.toLowerCase().replace(/\b\w/g, (c) => c.toUpperCase());
-}
-
-function createSlug(name: string) {
-  return name.toLowerCase().replace(/\s/g, "-");
-}
-
-const hospitals: Hospital[] = hospitalObjects.map((hospital: any) => {
-  const name = toTitleCase(hospital["Facility Name"]);
-  const city = toTitleCase(hospital["City/Town"]);
-  const address = toTitleCase(hospital["Address"]);
-  return {
-    name,
-    city,
-    state: hospital["State"],
-    address,
-    zipCode: hospital["ZIP Code"],
-    type: hospital["Hospital Type"],
-    cmsId: hospital["Facility ID"],
-    slug: createSlug(name),
-  };
-});
-
-console.log(hospitals[0]);
+await searchForPlace();
---
+++ parseCsv.ts
@@ -0,0 +1,51 @@
+import { parse } from "./deps.ts";
+
+export type GeoLocation = {
+  latitude: number;
+  longitude: number;
+};
+
+export type Hospital = {
+  name: string;
+  city: string;
+  state: string;
+  address: string;
+  zipCode: string;
+  type: string;
+  location?: GeoLocation; // Get from Google Maps API
+  cmsId: string;
+  imageUrl?: string; // Get from Google Maps, upload to Supabase storage, and add here
+  slug: string;
+};
+
+function toTitleCase(str: string) {
+  return str.toLowerCase().replace(/\b\w/g, (c) => c.toUpperCase());
+}
+
+function createSlug(name: string) {
+  return name.toLowerCase().replace(/\s/g, "-");
+}
+
+export async function parseHospitalData(): Hospital[] {
+  const hospitalsCsv = await Deno.readTextFile("./data/hospitals.csv");
+
+  const hospitalObjects = parse(hospitalsCsv, {
+    skipFirstRow: true,
+  });
+
+  return hospitalObjects.map((hospital: any) => {
+    const name = toTitleCase(hospital["Facility Name"]);
+    const city = toTitleCase(hospital["City/Town"]);
+    const address = toTitleCase(hospital["Address"]);
+    return {
+      name,
+      city,
+      state: hospital["State"],
+      address,
+      zipCode: hospital["ZIP Code"],
+      type: hospital["Hospital Type"],
+      cmsId: hospital["Facility ID"],
+      slug: createSlug(name),
+    };
+  });
+}
